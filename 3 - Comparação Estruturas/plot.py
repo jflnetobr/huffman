22cdf6987cedae7ff36579856661b77163d209be
João Pedro Cajueiro Marcolino <54550232+Jpcajueiro@users.noreply.github.com>
2020-01-06 00:40:39 -0300

    Pequenos ajustes
    Legenda e Marcadores adicionados.
    Pequena alteração no título do gráfico.

--- 3 - Comparação Estruturas/plot.py
+++ 3 - Comparação Estruturas/plot.py
@@ -6,14 +6,17 @@ style.use('ggplot')
 
 N,BST,AVL = np.loadtxt('compare_and_plot.txt',unpack=True,delimiter = ';')
 
-plt.plot(N,BST,'r')
-plt.plot(N,AVL,'b')
+plt.plot(N,BST,label='BST')
+plt.scatter(N,BST,marker='.', color='r')
+plt.plot(N,AVL,label='AVL')
+plt.scatter(N,AVL,marker='.', color='b')
 
-plt.title('BST vs AVL Tree Comparison')
+plt.title('Tree Comparison\nBST vs AVL')
 plt.xscale('log')
 plt.yscale('log')
 plt.ylabel('Comparisons')
 plt.xlabel('N')
+plt.legend()
 
 plt.show()
 
